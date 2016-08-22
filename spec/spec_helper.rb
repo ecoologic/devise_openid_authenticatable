c424bab87e97c9bbcb9705c7392fc1b0de90d292
ecoologic <erikecoologic@gmail.com>
2016-08-23 00:07:26 +1000

    rails5

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,11 @@ Webrat.configure do |config|
 end
 
 RSpec.configure do |config|
+  %i(controller view request).each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess,        type: type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
+    config.include ::Rails::Controller::Testing::Integration,        type: type
+  end if Rails.version >= '5'
   config.mock_with :mocha
   config.infer_spec_type_from_file_location!
 end
